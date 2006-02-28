267b64b4f82c9015ca7e749d023ba2b7104fab4b
Sebastian Felis <sebastian@phtagr.org>
2006-02-28 14:07:43 +0000

    - Add missing Edit.php. Edit changes all the IPTC data of an image.
    git-svn-id: https://svn.phtagr.org/phtagr/trunk@29 c01cf435-4f0c-0410-981c-aea1cd8e29bc

---
+++ phtagr/phtagr/Edit.php
@@ -0,0 +1,53 @@
+<?php
+
+global $prefix;
+include_once("$prefix/Iptc.php");
+
+class Edit 
+{
+
+function Edit()
+{
+  global $db;
+  global $auth;
+
+  if (!isset($_REQUEST['images']))
+    return;
+ 
+  foreach ($_REQUEST['images'] as $id)
+  {
+    $sql="SELECT filename 
+          FROM $db->image
+          WHERE id=$id";
+    $result=$db->query($sql);
+    if (!$result)
+      continue;
+
+    $row=mysql_fetch_row($result);
+    $filename=$row[0];
+    $iptc=new Iptc();
+    $iptc->load_from_file($filename);
+    
+    if (isset($_REQUEST['tags']))
+    {
+      $tags=split(" ", $_REQUEST['tags']);
+      $iptc->add_iptc_tags("2:025", $tags); 
+    }
+
+    //echo "<pre>\n";
+    //print_r($iptc);
+    //echo "</pre>\n";
+    if ($iptc->get_error()!='')
+      echo "<div class=\"warning\">IPTC error: ".$iptc->get_error()."<div/>\n";
+
+    if ($iptc->is_changed())
+    {
+      $iptc->save_to_file();
+      update_iptc($id, $auth->userid, $filename);  
+    }
+  }
+}
+
+
+}
+?>
