e5d2d727ee075c4feca3923a1aa5ebb0dc5766a5
Sebastian Felis <sebastian@phtagr.org>
2006-03-12 22:03:45 +0000

    - Fix. Remove doubled function definition in SectionUpload.php
    git-svn-id: https://svn.phtagr.org/phtagr/trunk@48 c01cf435-4f0c-0410-981c-aea1cd8e29bc

--- phtagr/phtagr/SectionUpload.php
+++ phtagr/phtagr/SectionUpload.php
@@ -36,7 +36,7 @@ function get_upload_filename($path, $filename)
 /** Adds after every third digit a dot, so the file size is
   easier to read 
   @return a string in which after every third digit comes a dot. */
-function get_readable_size ($size)
+function get_readable_size($size)
 {
   $tmp_size = $size;
   $size = "";
@@ -219,96 +219,6 @@ function zipfile_process($path, $filename)
   return true;
 }
 
-/** Adds after every third digit a dot, so the file size is
-  easier to read 
-  @return a string in which after every third digit comes a dot. */
-function get_readable_size ($size)
-{
-  $tmp_size = $size;
-  $size = "";
-  while ($tmp_size > 1000)
-  {
-    $block = fmod ($tmp_size , 1000);
-    $tmp_size = $tmp_size - $block;
-    $tmp_size = $tmp_size / 1000;
-
-    if ($size != "")
-      $size = sprintf ("%03d.$size", $block);
-    else
-      $size = sprintf ("%03d", $block);
-  } 
-  
-  $size = sprintf ("%3d.$size",$tmp_size);
-  return ($size);
-}
-
-/** Deletes an image in the upload folder
-  @returns true on success, false otherwise */
-function delete_upload()
-{
-  global $auth;
-  global $db;
-  global $prefix;
-
-  if (!$auth->is_auth || $auth->user!=='admin')
-  {
-    echo "You are not allowed to perform this action!\n";
-    return FALSE;
-  }
-
-  $pref = $db->read_pref();
-  $upload_dir = $pref['upload_dir'];
-  $fullpath = $upload_dir . "/" . $auth->user . "/";
-  $files = glob ( $fullpath . "*");
-
-  $found = FALSE;
-
-  if (count ($files ) == 0)
-  {
-    return FALSE;
-  }
-  if (is_dir ($fullpath))
-  {
-    if ($dh = opendir ($fullpath))
-    {
-      while (($file = readdir($dh)) !== false)
-      {
-        if ($file !== '.' && $file !== '..')
-        {
-	  /** Uhhh.... Bad hack! It is very weird, but when I try to
-	    reference the files directly over POST or GET, all the '.'
-	    in the filename get replaced by a '_'. This here check's
-	    whether there is a file that would be named like the other
-	    one if there was such an replacement. Any hints? */
-	  $tmp_name = $file;
-	  $tmp_name = preg_replace('/\./','_',$tmp_name);
-	  if (isset($_REQUEST[$tmp_name]))
-	  {
-	    $found = TRUE;
-            if (!unlink ($fullpath . $file))
-            {
-              /* We don't return a false, because maybe there is a
-	        file from a former installation. In this case, the
-	        file can be added through the browser */
-	      //return FALSE;
-            }
-            $sql = "DELETE FROM ".$db->image." 
-	            WHERE filename='".$fullpath . $file ."'";
-            if (!$db->query($sql))
-            {
-              return FALSE;
-            }
-	  }
-        }
-      }
-      if ($found)
-        return TRUE;
-    }
-  }
-
-  return FALSE;
-}
-
 /** Upload all images 
   @return true on success, false otherwise */
 function upload_process()
@@ -457,88 +367,6 @@ function print_uploaded()
       echo "\t\t\t<td</td>\n";
       echo "\t\t\t<td align=\"right\">"
 	 . $this->get_readable_size ( $file_size_sum )
-	 . " Bytes</td>\n";
-      echo "\t\t</tr>\n";
-
-      closedir ($dh);
-    }
-    else
-    {
-      echo "Couldn't open dir: $fullpath\n";
-    }
-  }
-  else
-  {
-    echo "$fullpath doesn't seem to be a directory\n";
-  }
-  echo "</table>\n";
-  echo "\t<input type=\"submit\" value=\"Delete\" class=\"delete\" />\n";
-  echo "</form>\n";
-
-}
-
-
-/** Lists all the already uploaded images
-  @return nicely formatted html list of the uploaded images */
-function print_uploaded()
-{
-  global $auth;
-  global $prefix;
-  global $db;
-
-  # Now we want to list all files, the user has already uploaded
-  $pref = $db->read_pref();
-  $upload_dir = $pref['upload_dir'];
-  $fullpath = $upload_dir . "/" . $auth->user . "/";
-  $files = glob ( $fullpath . "*");
-  if (count ($files) == 0)
-    return;
-  sort ($files, SORT_STRING);
-
-  echo "<h2>Available images</h2>\n";
-  echo "<form action=\"./index.php\" method=\"POST\">\n";
-  echo "\t<input type=\"hidden\" name=\"section\" value=\"upload\" />\n";
-  echo "\t<input type=\"hidden\" name=\"delete_upload\" />\n";
-  echo "\t<table border=\"1\">\n";
-  echo "\t<th width=\"5%\"></th>\n";
-  echo "\t<th width=\"5%\">Image</th>\n";
-  echo "\t<th>Name</th>\n";
-  echo "\t<th width=\"20%\">Size</th>\n";
-  echo "\t<th width=\"1%\"></th>\n";
-
-  $filesum = 0;
-  if (is_dir ($fullpath))
-  {
-    if ($dh = opendir ($fullpath))
-    {
-      while (($file = readdir($dh)) !== false)
-      {
-        if ($file !== '.' && $file !== '..')
-        {
-          $sql = "SELECT id FROM ".$db->image."
-	          WHERE filename='". $fullpath . $file . "'";
-          $result = $db->query($sql);
-          $v = mysql_fetch_array ($result, MYSQL_ASSOC);
-
-          echo "\t\t<tr>\n";
-	  echo "\t\t\t<td align=\"center\" width=\"5\"><input type=\"checkbox\" name=\"".$file."\" /></td>\n";
-          echo "\t\t\t<td align=\"center\">\n";
-          echo print_mini($v['id']) . "</td>\n";
-          echo "\t\t\t<td align=\"center\">$file</td>\n";
-          echo "\t\t\t<td align=\"right\">"
-             . $this->get_readable_size ( filesize($fullpath . $file) ) 
-             . " Bytes</td>\n";
-	  echo "<td><div class=\"headerright\"><a href='./index.php?section=upload&delete_upload=&".urlencode($file)."=on'>Delete</a></div></td>\n";
-          echo "\t\t</tr>\n";
-          $filesum += filesize($fullpath . $file) ;
-        }
-      }
-      echo "\t\t<tr>\n";
-      echo "\t\t\t<td></td>\n";
-      echo "\t\t\t<td></td>\n";
-      echo "\t\t\t<td</td>\n";
-      echo "\t\t\t<td align=\"right\">"
-	 . $this->get_readable_size ( $filesum )
 	 . " Bytes</td>\n";
       echo "\t\t</tr>\n";
 
