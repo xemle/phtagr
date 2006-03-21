4d85c8a08c74dc1d1e5e84231da886e9539679bd
Sebastian Felis <sebastian@phtagr.org>
2006-03-21 23:15:15 +0000

    - Improve the javascript. Now it uses a global variable to store the old content of an element.
    git-svn-id: https://svn.phtagr.org/phtagr/trunk@57 c01cf435-4f0c-0410-981c-aea1cd8e29bc

--- phtagr/js/forms.js
+++ phtagr/js/forms.js
@@ -1,29 +1,49 @@
-function reset_text(nodeid, text)
+/** Define global variable */
+var Data=new Array();
+
+/** Reset a node by the text
+  @param id Id of the element
+  @param text New text of the node. The text is encoded in B64 */
+function reset_text(id)
 {
-  var e=document.getElementById(nodeid);
+  var e=document.getElementById(id);
+  if (e==null)
+    return;
   // decode node content from b64
-  e.innerHTML=atob(text);
+  e.innerHTML=Data[id];
 }
 
+/** Add a form for tags
+  @param id ID of the image
+  @param tags List of the tags */
 function add_form_tags(id, tags)
 {
   var node=id+"-tag";
   var e=document.getElementById(node);
+  if (e==null)
+    return;
+
   var i=id+"-edit";
   var text=e.innerHTML;
 
+  //text_enc=btoa(text); 
   // Does a form already exists?
-  if (text.substr(0,5)=="<form")
+  // On mozilla, the form will be omitted, check also for the next input node
+  if (Data[node]!=null && text!=Data[node])
+  {
+    reset_text(node);
     return;
+  }
 
+  // Remember old content
+  Data[node]=text;
   // encode node content to b64 to catch all special characters
-  text=btoa(text); 
   e.innerHTML="<form action=\"index.php\" method=\"post\">" +
     "<input type=\"hidden\" name=\"image\" value=\""+id+"\"/>"+
     "<input id=\"" + i + "\" type=\"text\" name=\"js_tags\" value=\"" + tags + "\" size=\"35\"/>"+
     "<br/>"+
     "<input class=\"submit\" type=\"submit\" value=\"Send\"/> or "+
-    "<input class=\"reset\" type=\"reset\" onclick=\"reset_text('"+node+"','"+text+"')\"/>"+
+    "<input class=\"reset\" type=\"reset\" onclick=\"reset_text('"+node+"')\"/>"+
   "</form>";
   document.getElementById(i).focus();
 }
--- phtagr/phtagr/PageBase.php
+++ phtagr/phtagr/PageBase.php
@@ -2,43 +2,43 @@
 
 class PageBase
 {
-    var $title;
-    var $sections;
+  var $title;
+  var $sections;
 
-    function PageBase()
-    {
-        $this->title="phTagr";
-        $this->sections=array();
-    }
+  function PageBase()
+  {
+    $this->title="phTagr";
+    $this->sections=array();
+  }
 
-    function add_section($section)
-    {
-        array_push($this->sections, $section);
-    }
+  function add_section($section)
+  {
+    array_push($this->sections, $section);
+  }
 
-    function print_header_html()
+  function print_header_html()
+  {
+    echo "<html>\n";
+    echo "<head>\n";
+    echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"themes/default/style.css\"/>\n";
+    echo "<title>$this->title</title>\n";
+    echo "<script language=\"JavaScript\" src=\"js/forms.js\" type=\"text/javascript\"></script>\n";
+    echo "</head>\n\n";
+  }
+  
+  function print_footer_html()
+  {
+    echo "</html>";
+  }
+  
+  function layout()
+  {
+    $this->print_header_html();
+    foreach ($this->sections as $section)
     {
-        echo "<html>\n";
-        echo "<head>\n";
-        echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"themes/default/style.css\"/>\n";
-        echo "<title>$this->title</title>\n";
-        echo "<script language=\"JavaScript\" src=\"js/forms.js\" type=\"text/javascript\"></script>\n";
-        echo "</head>\n\n";
+      $section->print_section();
     }
-    
-    function print_footer_html()
-    {
-        echo "</html>";
-    }
-    
-    function layout()
-    {
-        $this->print_header_html();
-        foreach ($this->sections as $section)
-        {
-            $section->print_section();
-        }
-        $this->print_footer_html();
-    }
+    $this->print_footer_html();
+  }
 }
 ?>
