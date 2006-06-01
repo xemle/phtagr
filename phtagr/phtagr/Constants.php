440c627bf42a8fabfa0292b6a4da9746bbf66fbf
Sebastian Felis <sebastian@phtagr.org>
2006-06-01 05:51:00 +0000

    - Fix: Commit missing file Constants.php
    git-svn-id: https://svn.phtagr.org/phtagr/trunk@81 c01cf435-4f0c-0410-981c-aea1cd8e29bc

---
+++ phtagr/phtagr/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+// ACL constants
+define("ACL_DOWNLOAD", 0x80);
+define("ACL_FULLSIZE", 0x40);
+define("ACL_HIGHSOLUTION", 0x20);
+define("ACL_PREVIEW", 0x10);
+define("ACL_METADATA", 0x02);
+define("ACL_EDIT", 0x01);
+
+?>
