d50a6ff37d1ca1b5110c1e3b2026e91f5d05ecc3
Sebastian Felis <sebastian@phtagr.org>
2009-04-22 10:56:36 +0000

    - UpgradeMediaSchemaShell: Add check for upgrade - Fix home controller for media model
    git-svn-id: https://svn.phtagr.org/phtagr/branches/phtagr-media-schema@415 c01cf435-4f0c-0410-981c-aea1cd8e29bc

--- vendors/shells/upgradeMediaSchema.php
+++ vendors/shells/upgradeMediaSchema.php
@@ -28,7 +28,6 @@ class UpgradeMediaSchemaShell extends Shell {
   var $uses = array('Media', 'MyFile');
   var $db = null;
   var $Logger = null;
-  var $_dryRun = false;
 
   function initialize() {
     $this->Logger =& new LoggerComponent();
@@ -48,31 +47,48 @@ class UpgradeMediaSchemaShell extends Shell {
 
   function _execute($sql) {
     $this->Logger->debug($sql);
-    if (!$this->_dryRun) {
-      $this->UpgradeSchema->db->execute($sql);
-    }
+    $this->UpgradeSchema->db->execute($sql);
   }
 
+  /** Returns the table name with table prefix */
   function _getTable($name) {
     return $this->UpgradeSchema->db->config['prefix'].$name;
   }
 
-  function _renameTable($from, $to) {
-    $sql = "ALTER TABLE `".$this->_getTable($from)."` RENAME `".$this->_getTable($to)."`";
+  /** Renames a table 
+    @param old Old table name
+    @param new New table name */
+  function _renameTable($old, $new) {
+    $sql = "ALTER TABLE `".$this->_getTable($old)."` RENAME `".$this->_getTable($new)."`";
     $this->_execute($sql);
   }
 
-  function _renameColumn($table, $from, $to) {
-    $colDef = $this->UpgradeSchema->schema->tables[$table][$to];
-    $colDef['name'] = $to;
-    $sql = "ALTER TABLE `".$this->_getTable($table)."` CHANGE `".$from."` ";
+  /** Renames a column 
+    @param old Old column name
+    @param new New column name */
+  function _renameColumn($table, $old, $new) {
+    $colDef = $this->UpgradeSchema->schema->tables[$table][$new];
+    $colDef['name'] = $new;
+    $sql = "ALTER TABLE `".$this->_getTable($table)."` CHANGE `".$old."` ";
     $sql .= $this->UpgradeSchema->db->buildColumn($colDef);
     $this->_execute($sql);
   }
 
-  function _prepareUpgrade() {
+  /** Checks if the instance requires an upgrade
+    @return True if an upgrade is required */
+  function _requiresUpgrade() {
     $this->UpgradeSchema->initDataSource();
     $this->UpgradeSchema->loadSchema();
+
+    if ($this->UpgradeSchema->hasTables(array('media', 'files'))) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /** Prepares the upgrade by renaming tables and columns */
+  function _prepareUpgrade() {
     // rename tables
     $this->_renameTable('images', 'media');
     $this->_renameTable('categories_images', 'categories_media');
@@ -88,10 +104,12 @@ class UpgradeMediaSchemaShell extends Shell {
     // no rename for properties and locks to ensure data migration
   }
 
+  /** Finds an external video thumb and adds it to the table 
+    @param media Media model data
+    @param file File model data */
   function _addVideoThumb($media, $file) {
     $name = $file['File']['file'];
     $pattern = substr($name, 0, strrpos($name, '.')+1)."[Tt][Hh][Mm]";
-    $this->Logger->debug($pattern);
     $folder =& new Folder();
     $folder->cd($file['File']['path']);
     $found = $folder->find($pattern);
@@ -109,79 +127,105 @@ class UpgradeMediaSchemaShell extends Shell {
     }
   }
 
+  /** Migrate a single media 
+    @param media Media model data 
+    @return True on success. False on errors */
+  function _migrateMedia($media) {
+    // create file model
+    $filename = $media['Media']['path'].$media['Media']['file'];
+    $file = $this->MyFile->create($filename, $media['Media']['user_id']);
+    $file['File']['media_id'] = $media['Media']['id'];
+    $file['File']['readed'] = date("Y-m-d H:i:s", filemtime($filename));
+    if (!$this->MyFile->save($file)) {
+      $this->Logger->debug("Cannot migrate data from media {$media['Media']['id']}");
+      $this->Logger->warn($file);
+      return false;
+    } 
+
+    // migrate properties and locks
+    $fileId = $this->MyFile->getLastInsertId();
+    $file['File']['id'] = $fileId;
+    $sql = "UPDATE ".$this->_getTable('properties')." SET file_id = $fileId WHERE image_id = {$media['Media']['id']}";
+    $this->_execute($sql);
+    $sql = "UPDATE ".$this->_getTable('locks')." SET file_id = $fileId WHERE image_id = {$media['Media']['id']}";
+    $this->_execute($sql);
+
+    // set media type
+    $type = $file['File']['type'];
+    switch ($type) {
+      case FILE_TYPE_IMAGE:
+        $this->Media->setType($media, MEDIUM_TYPE_IMAGE);
+        break;
+      case FILE_TYPE_VIDEO:
+        // if video search for thumb and add it
+        $this->Media->setType($media, MEDIUM_TYPE_VIDEO);
+        $this->_addVideoThumb($media, $file);
+        break;
+      default:
+        $this->Logger->warn("Unhandled file type $type");
+    }
+    return true;
+  }
+
+  /** Migrates the the old data to the new data schema. Creates file models for
+    * each media */
   function _migrateData() {
     // init media and file model
     $this->Media =& new Media();
     $this->MyFile =& new MyFile();
-    // foreach media
+
+    // fetch all media
     $this->Media->unbindAll();
     $media = $this->Media->findAll("1 = 1", array('Media.id', 'Media.path', 'Media.file', 'Media.user_id', 'Media.flag'));
     $this->out("Migrate ".count($media)." media...");
-    $this->Logger->verbose("Migrate ".count($media)." media");
-    $errors = 0;
-    foreach ($media as $media) {
-      //  create file model
-      $filename = $media['Media']['path'].$media['Media']['file'];
-      $file = $this->MyFile->create($filename, $media['Media']['user_id']);
-      $file['File']['media_id'] = $media['Media']['id'];
-      $file['File']['readed'] = date("Y-m-d H:i:s", filemtime($filename));
-      if (!$this->MyFile->save($file)) {
-        $this->Logger->debug("Cannot migrate data from media {$media['Media']['id']}");
-        $this->Logger->warn($file);
-        $errors++;
-        continue;
-      } 
-      // migrate properties and locks
-      $fileId = $this->MyFile->getLastInsertId();
-      $file['File']['id'] = $fileId;
-      $sql = "UPDATE ".$this->_getTable('properties')." SET file_id = $fileId WHERE image_id = {$media['Media']['id']}";
-      $this->_execute($sql);
-      $sql = "UPDATE ".$this->_getTable('locks')." SET file_id = $fileId WHERE image_id = {$media['Media']['id']}";
-      $this->_execute($sql);
+    $this->Logger->verbose("Found ".count($media)." media to migrade ...");
 
-      // set media type
-      $type = $file['File']['type'];
-      switch ($type) {
-        case FILE_TYPE_IMAGE:
-          $this->Media->setType($media, MEDIUM_TYPE_IMAGE);
-          break;
-        case FILE_TYPE_VIDEO:
-          // if video search for thumb and add it
-          $this->Media->setType($media, MEDIUM_TYPE_VIDEO);
-          $this->_addVideoThumb($media, $file);
-          break;
-        default:
-          $this->Logger->warn("Unhandled file type $type");
+    $errors = 0;
+    foreach ($media as $m) {
+      if (!$this->_migrateMedia($m)) {
+        $errors++;
       }
     }
     if ($errors) {
       $this->out("Upgrade ".count($media)." media with $errors errors");
       $this->Logger->err("Upgrade ".count($media)." media with $errors errors");
+      return true;
     } else {
-      $this->Logger->info("Upgrade ".count($media)." media without errors");
+      $this->Logger->info("Upgrade ".count($media)." media successfully");
+      return false;
     }
   }
 
   function upgrade() {
-    // @todo add parameters for base path, config or tmp directory
-    // @todo check tables for upgrade
+    if (!$this->_requiresUpgrade()) {
+      $this->out("Hey! No upgrade required ;-)");
+      return;
+    }
     $this->out("Prepare upgrade...");
     // upgrade database
     $this->_prepareUpgrade();
-    //  automatic create missing tables/columns
+
+    // automatic create missing tables/columns
     $this->UpgradeSchema->deleteModelCache();
     $this->out("Upgrade schema...");
     $this->UpgradeSchema->upgrade(true);
+
+    // migrate data
+    $error = false;
+    $this->UpgradeSchema->deleteModelCache();
+    $error = $this->_migrateData();
+
+    $this->out("Finalizing upgrade...");
     // delete model cache dir
     $this->UpgradeSchema->deleteModelCache();
-    $this->_migrateData();
-    // downgrade database
-    //  drop not required tables/columns
-    // delete model cache dir
-    $this->out("Finalize schema...");
-    $this->UpgradeSchema->deleteModelCache();
+    // downgrade database and drop not required tables/columns
     $this->UpgradeSchema->upgrade();
-    $this->out("Done.");
+
+    if (!$error) {
+      $this->out("All done. Enjoy!");
+    } else {
+      $this->out("Mmm. Something went wrong. Use your backup and look logs for details");
+    }
   }
 
   function help() {
