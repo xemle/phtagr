9abfd2a552aab5f4ce8a3ed7026a15bc7e5ad6ee
Xemle <xemle@phtagr.org>
2011-02-26 17:04:34 +0100

    Add embedded media on email notifications

---
+++ controllers/components/image_email.php
@@ -0,0 +1,177 @@
+<?php 
+/*
+ * phtagr.
+ * 
+ * social photo gallery for your community.
+ * 
+ * Copyright (C) 2006-2010 Sebastian Felis, sebastian@phtagr.org
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2 of the 
+ * License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+App::import('Component', 'Email');
+
+class ImageEmailComponent extends EmailComponent {
+  
+  /** List of embedded images */
+  var $images = array();
+
+  function initialize(&$controller) {
+    parent::initialize(&$controller);
+  }
+
+  function _createHeader() {
+    // fake attachment to force MIME mixed
+    $this->attachments[-1] = false;
+    parent::_createHeader();
+  }
+
+  /** Add support of related content */
+	function _render($content) {
+		$viewClass = $this->Controller->view;
+
+		if ($viewClass != 'View') {
+			list($plugin, $viewClass) = pluginSplit($viewClass);
+			$viewClass = $viewClass . 'View';
+			App::import('View', $this->Controller->view);
+		}
+
+		$View = new $viewClass($this->Controller);
+		$View->layout = $this->layout;
+		$msg = array();
+
+		$content = implode("\n", $content);
+
+		if ($this->sendAs === 'both') {
+			$htmlContent = $content;
+			if (!empty($this->attachments)) {
+				$msg[] = '--' . $this->__boundary;
+				$msg[] = 'Content-Type: multipart/alternative; boundary="alt-' . $this->__boundary . '"';
+				$msg[] = '';
+			}
+			$msg[] = '--alt-' . $this->__boundary;
+			$msg[] = 'Content-Type: text/plain; charset=' . $this->charset;
+			$msg[] = 'Content-Transfer-Encoding: 7bit';
+			$msg[] = '';
+
+			$content = $View->element('email' . DS . 'text' . DS . $this->template, array('content' => $content), true);
+			$View->layoutPath = 'email' . DS . 'text';
+			$content = explode("\n", $this->textMessage = str_replace(array("\r\n", "\r"), "\n", $View->renderLayout($content)));
+
+			$msg = array_merge($msg, $content);
+
+			$msg[] = '';
+			$msg[] = '--alt-' . $this->__boundary;
+      if (!empty($this->images)) {
+        $msg[] = 'Content-Type: multipart/related; boundary="rel-' . $this->__boundary . '"';
+        $msg[] = '';
+        $msg[] = '--rel-' . $this->__boundary;
+      }
+			$msg[] = 'Content-Type: text/html; charset=' . $this->charset;
+			$msg[] = 'Content-Transfer-Encoding: 7bit';
+			$msg[] = '';
+
+			$htmlContent = $View->element('email' . DS . 'html' . DS . $this->template, array('content' => $htmlContent), true);
+			$View->layoutPath = 'email' . DS . 'html';
+			$htmlContent = explode("\n", $this->htmlMessage = str_replace(array("\r\n", "\r"), "\n", $View->renderLayout($htmlContent)));
+			$msg = array_merge($msg, $htmlContent);
+      if (!empty($this->images)) {
+        $images = $this->_attachImages();
+        $msg = array_merge($msg, $images);
+        $msg[] = '';
+        $msg[] = '--rel-' . $this->__boundary . '--';
+        $msg[] = '';
+      }
+			$msg[] = '';
+			$msg[] = '--alt-' . $this->__boundary . '--';
+			$msg[] = '';
+
+			ClassRegistry::removeObject('view');
+			return $msg;
+		}
+
+		if (!empty($this->attachments)) {
+			if ($this->sendAs === 'html') {
+				$msg[] = '';
+				$msg[] = '--' . $this->__boundary;
+				$msg[] = 'Content-Type: text/html; charset=' . $this->charset;
+				$msg[] = 'Content-Transfer-Encoding: 7bit';
+				$msg[] = '';
+			} else {
+				$msg[] = '--' . $this->__boundary;
+				$msg[] = 'Content-Type: text/plain; charset=' . $this->charset;
+				$msg[] = 'Content-Transfer-Encoding: 7bit';
+				$msg[] = '';
+			}
+		}
+
+		$content = $View->element('email' . DS . $this->sendAs . DS . $this->template, array('content' => $content), true);
+		$View->layoutPath = 'email' . DS . $this->sendAs;
+		$content = explode("\n", $rendered = str_replace(array("\r\n", "\r"), "\n", $View->renderLayout($content)));
+
+		if ($this->sendAs === 'html') {
+			$this->htmlMessage = $rendered;
+		} else {
+			$this->textMessage = $rendered;
+		}
+
+		$msg = array_merge($msg, $content);
+		ClassRegistry::removeObject('view');
+
+		return $msg;
+	}
+
+  /** Add support of Content-ID */
+  function _attachImages() {
+    $files = array();
+    foreach ($this->images as $filename => $options) {
+      if (!is_array($options)) {
+        $options['file'] = $options;
+      }
+      $options = am(array('mime' => 'application/octet-stream', 'id' => false, 'file' => false), $options);
+      $file = $this->_findFiles($options['file']);
+      if (!empty($file)) {
+        if (is_int($filename)) {
+          $filename = basename($file);
+        }
+        $options['file'] = $file;
+        $files[$filename] = $options;
+      }
+    }
+
+    $msg = array();
+    foreach ($files as $filename => $options) {
+      $file = $options['file'];
+      $handle = fopen($file, 'rb');
+      $data = fread($handle, filesize($file));
+      $data = chunk_split(base64_encode($data)) ;
+      fclose($handle);
+
+      $msg[] = '--rel-' . $this->__boundary;
+      $msg[] = 'Content-Type: ' . $options['mime'];
+      $msg[] = 'Content-Transfer-Encoding: base64';
+      if ($options['id']) {
+        $msg[] = 'Content-ID: <' . $options['id'] . '>';
+      }
+      $msg[] = 'Content-Disposition: attachment; filename="' . basename($filename) . '"';
+      $msg[] = '';
+      $msg = array_merge($msg, explode("\r\n", $data)); 
+      $msg[] = '';
+    }
+    Logger::debug($msg);
+    return $msg;
+  }
+}
+?>
--- vendors/shells/notify.php
+++ vendors/shells/notify.php
@@ -21,42 +21,44 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-App::import('Model', array('User', 'Media'));
-App::import('Core', array('Controller'));
-App::import('Component', array('Email', 'Search'));
+App::import('Model', array('User', 'Media', 'MyFile'));
+App::import('Core', array('Controller', 'Router'));
+App::import('Controller', array('AppController'));
+App::import('Component', array('ImageEmail', 'Search', 'PreviewManager'));
 App::import('File', 'Logger', array('file' => APP . 'logger.php'));
 
-class AppControllerMock extends Controller {
-  var $uses = array('User', 'Media');
-  var $components = array('Email', 'Search');
+class AppControllerMock extends AppController {
+  var $uses = array('User', 'Media', 'MyFile');
+  var $components = array('ImageEmail', 'Search', 'PreviewManager');
   var $user = null;
   
   function startup() {
     $this->constructClasses();
     $this->Component->_loadComponents(&$this);
     $this->Component->initialize(&$this);
+    $this->PreviewManager->Media =& $this->Media;
     $this->_configureEmail();
   }
 
   /** Configure email component on any SMTP configuration values in core.php */
   function _configureEmail() {
     if (Configure::read('Mail.from')) {
-      $this->Email->from = Configure::read('Mail.from');
+      $this->ImageEmail->from = Configure::read('Mail.from');
     } else {
-      $this->Email->from = "phTagr <noreply@phtagr.org>";
+      $this->ImageEmail->from = "phTagr <noreply@phtagr.org>";
     }
     if (Configure::read('Mail.replyTo')) {
-      $this->Email->replyTo = Configure::read('Mail.replyTo');
+      $this->ImageEmail->replyTo = Configure::read('Mail.replyTo');
     }
     $names = array('host', 'port', 'username', 'password');
     foreach($names as $name) {
       $value = Configure::read("Smtp.$name");
       if ($value) {
-        $this->Email->smtpOptions[$name] = $value;
+        $this->ImageEmail->smtpOptions[$name] = $value;
       }
     }
-    if (!empty($this->Email->smtpOptions['host'])) {
-      $this->Email->delivery = 'smtp';
+    if (!empty($this->ImageEmail->smtpOptions['host'])) {
+      $this->ImageEmail->delivery = 'smtp';
     }
   }
   
@@ -83,14 +85,14 @@ class NotifyShell extends Shell {
     $this->Controller->startup();
     foreach($this->Controller->uses as $model) {
       if (empty($this->Controller->{$model})) {
-        $this->out("Could not use model $model");
+        $this->out("Could not use model '$model'");
         exit(1);
       }  
       $this->{$model} =& $this->Controller->{$model};
     }
-    foreach($this->Controller->components as $component) {
+    foreach($this->Controller->components as $component => $config) {
       if (empty($this->Controller->{$component})) {
-        $this->out("Could not use model $component");
+        $this->out("Could not use component '$component'");
         exit(1);
       }  
       $this->{$component} =& $this->Controller->{$component};
@@ -119,16 +121,31 @@ class NotifyShell extends Shell {
     exit();
   }
 
+  function _buildImages($media) {
+    $this->ImageEmail->images = array();
+    foreach ($media as $m) {
+      $file = $this->Controller->PreviewManager->getPreview($m, 'mini');
+      if (!$file) {
+        Logger::err("Could not create preview for media {$m['Media']['id']}");
+        continue;
+      }
+      $attachment = array('file' => $file, 'id' => 'media-' . $m['Media']['id'] . '.jpg', 'mime' => 'image/jpeg');
+      $this->ImageEmail->images[] = $attachment;
+    }
+  }
+
   function _sendNotifaction($user, $media) {
-    $this->Email->to = $user['User']['email'];
-    $this->Email->subject = "New media are available";
-    $this->Email->template = 'notify_new_media';
+    $this->ImageEmail->to = $user['User']['email'];
+    $this->ImageEmail->subject = "New media are available";
+    $this->ImageEmail->template = 'notify_new_media';
+    $this->ImageEmail->sendAs = 'both';
 
     $this->Controller->set('user', $user);
     $this->Controller->set('media', $media);
     $this->Controller->set('url', $this->url);
+    $this->_buildImages($media);
 
-    if (!$this->Email->send()) {
+    if (!$this->ImageEmail->send()) {
       $this->out("Could not send new media notification email to {$user['User']['email']}");
       Logger::err("Could not send new media notification email to {$user['User']['email']}");
     } else {
@@ -168,7 +185,7 @@ class NotifyShell extends Shell {
       }
       $this->Controller->user =& $user;
 
-      $this->Search->setShow(24);
+      $this->Search->setShow(12);
       $this->Search->setSort('newest');
       $this->Search->setExcludeUser($user['User']['id']);
       $this->Search->setCreatedFrom($user['User']['last_notify']);
