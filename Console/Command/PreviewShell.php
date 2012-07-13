37c8a920ff5087438ea2148556a561eb65427e9c
Xemle <xemle@phtagr.org>
2012-07-13 23:45:51 +0200

    Fix parameter parsing for shell scripts
    Update shell parameter parsing for CakePHP 2.0

--- Console/Command/PreviewShell.php
+++ Console/Command/PreviewShell.php
@@ -30,40 +30,27 @@ class PreviewShell extends AppShell {
     $this->mockUser($mockUser);
   }
 
-  function startup() {
-  }
-
-  function main() {
-    $this->help();
+  public function getOptionParser() {
+    $parser = parent::getOptionParser();
+    $parser->addOption('max', array(
+      'help' => __('Maximum generation count. Default is 10. Use 0 to generate all previews')
+    ))->addOption('start-chunk', array(
+      'help' => __('Set the start chunk number. A chunk has a size of 100 media. Default is 1')
+    ))->addOption('size', array(
+      'help' => __('Set the minimum preview size. Default is thumb'),
+      'choices' => $this->sizes
+    ))->addOption('user', array(
+      'help' => __('Generate only previews for given user')
+    ))->addSubcommand('generate', array(
+      'help' => __('Create preview files')
+    ))->description(__('Generate preview files'));
+    return $parser;
   }
 
   function verboseOut($msg) {
     if ($this->verbose) {
       $this->out($msg);
     }
-  }
-
-  function help() {
-    $this->out("Help screen");
-    $this->hr();
-    $this->out("This shell generates preview images in batch mode.");
-    $this->out("");
-    $this->out("generate");
-    $this->out("\tGenerate previews.");
-    $this->hr();
-    $this->out("Options:");
-    $this->out("-max count");
-    $this->out("\tMaximum generation count. Default is 10. Use 0 to generate all previews.");
-    $this->out("-start-chunk number");
-    $this->out("\tSet the start chunk number. A chunk has a size of 100 media. Default is 1.");
-    $this->out("-size (mini|thumb|preview|high)");
-    $this->out("\tSet the minimum preview size. Default is thumb.");
-    $this->out("-user username");
-    $this->out("\tGenerate only previews for given user.");
-    $this->out("-verbose");
-    $this->out("\tBe verbose");
-    $this->hr();
-    exit();
   }
 
   function generate() {
