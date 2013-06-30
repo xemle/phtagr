5ce279082534a942e212d19e8379fd8b64e977d6
Xemle <xemle@phtagr.org>
2013-06-30 22:01:27 +0200

    Fix migration mappings

--- Config/Migration/map.php
+++ Config/Migration/map.php
@@ -1,10 +1,10 @@
 <?php
 $map = array(
-	1 => array('001-initial-schema' => 'M4d1f861d09384b5d91c01828cbdd56cb'),
-	2 => array('002-fix-unzip-path' => 'Md506e1993bdc0017e4f11fd8e3210ec7'),
-	3 => array('003-drop-habtm-primary-keys' => 'Mc15b41fcc828e7dbb7b8e3fd541d79d6'),
-	4 => array('004-multiple-access-groups' => 'M57acac5f420bd77f6d55a446199ccab6'),
-	5 => array('005-table-media-drop-column-group-id' => 'M6cac9a91a1109f703aff4297a71a0f52'),
-	6 => array('006-one-meta-data-table' => 'Mffcaea2061011ff9181c79c14fa83622'),
+	1 => array('001_initial_schema' => 'M4d1f861d09384b5d91c01828cbdd56cb'),
+	2 => array('002_fix_unzip_path' => 'Md506e1993bdc0017e4f11fd8e3210ec7'),
+	3 => array('003_drop_habtm_primary_keys' => 'Mc15b41fcc828e7dbb7b8e3fd541d79d6'),
+	4 => array('004_multiple_access_groups' => 'M57acac5f420bd77f6d55a446199ccab6'),
+	5 => array('005_table_media_drop_column_group_id' => 'M6cac9a91a1109f703aff4297a71a0f52'),
+	6 => array('006_one_meta_data_table' => 'Mffcaea2061011ff9181c79c14fa83622'),
 );
 ?>
