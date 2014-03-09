3daf426defcbb46450382821c975158ea56c9933
Xemle <xemle@phtagr.org>
2014-03-09 20:47:00 +0100

    Cleanup of MenuHelper.php

--- View/Elements/menu.ctp
+++ View/Elements/menu.ctp
@@ -1,50 +1,5 @@
+<ul>
 <?php
-  $controller = $this->params['controller'];
-  $action = $this->params['action'];
-  $items = array();
-  $items[] = array(
-    'text' => __('Home'),
-    'link' => '/',
-    'type' => ($controller == 'home'?'current':''));
-  if ($this->Session->check('User.id')) {
-    $userId = $this->Session->read('User.id');
-    $role = $this->Session->read('User.role');
-    $myImages = false;
-    if (isset($this->Search) &&
-      ($controller == 'explorer' || $controller == 'images') &&
-      $this->Search->getUser() == $this->Session->read('User.username')) {
-      $myImages = true;
-    }
-    $items[] = array(
-      'text' => __('Explorer'),
-      'link' => '/explorer',
-      'type' => ($controller == 'explorer' && !$myImages?'current':''));
-
-    if ($role >= ROLE_GUEST) {
-      $items[] = array('text' =>
-        __('My Photos'),
-        'link' => "/explorer/user/".$this->Session->read('User.username'),
-        'type' => ($controller == 'explorer' && $myImages?'current':''));
-    }
-    if ($role >= ROLE_USER) {
-      if (!$this->Option->get('user.browser.full', 0)) {
-        $items[] = array(
-          'text' => __('Upload'),
-          'link' => '/browser/quickupload',
-          'type' => ($controller == 'browser' && $action == 'quickupload'?'current':''));
-      } else {
-        $items[] = array(
-          'text' => __('My Files'),
-          'link' => '/browser',
-          'type' => ($controller == 'browser' && $action != 'quickupload'?'current':''));
-      }
-    }
-  } else {
-    $items[] = array(
-      'text' => __('Explorer'),
-      'link' => '/explorer',
-      'type' => ($controller == 'explorer'?'current':''));
-  }
-
-  echo $this->Menu->getMainMenu(array('id' => false, 'title' => false, 'items' => $items));
+  echo $this->Menu->renderMenu('main-menu');
 ?>
+</ul>
--- View/Helper/MenuHelper.php
+++ View/Helper/MenuHelper.php
@@ -14,186 +14,15 @@
  * @since         phTagr 2.2b3
  * @license       GPL-2.0 (http://www.opensource.org/licenses/GPL-2.0)
  */
-class MenuHelper extends AppHelper
-{
+class MenuHelper extends AppHelper {
+
   var $helpers = array('Html', 'Session');
-
-  var $_url = false;
-
-/**
-  items
-    text: Link text
-    link: Link
-    type: menu types: current, active, text, multi, or false
-    onmouseover, onmouseout: add event
-    submenu: subitem, array of items
-
-  Example
-  array(
-    'id' => 'list id',
-    'title' => 'Title text',
-    'items' => array(
-      [0] => array(
-        'text' => 'TEXT'
-        'type' => 'active'
-        ),
-      [1] => array(
-        'text' => 'Submenu Text',
-        'type' => 'text',
-        'submenu' => array(
-          'items' => array(
-            [0] => array(
-              'text' => 'Submenu Text',
-              'link' => '#submenulink'
-              )
-            )
-          )
-        ),
-      [2] => array(
-        'text' => 'Menu Text'
-        'link' => '#link'
-        ),
-      [3] => array(
-        'text' => '#link1 #link2'
-        'type' => 'multi'
-        'onmouseover' => 'toggleItem('item-3');'
-        )
-      )
-    )
-*/
-  function _getMenu($data) {
-    $data = am(array('id' => false, 'title' => false, 'items' => null, 'toggleid' => false), $data);
-    if ($data['id'])
-      $out = "<ul id=\"{$data['id']}\">\n";
-    else
-      $out = "<ul>\n";
-
-    if ($data['title'])
-      $out .= "<li id=\"title\">{$data['title']}</li>\n";
-
-    if (empty($data['items']))
-      return $out."</ul>\n";
-
-    foreach ($data['items'] as $item) {
-      $item = am(array('type' => false, 'text' => false, 'link' => false), $item);
-      if (!$item['text']) {
-        // Trigger error
-        continue;
-      }
-
-      if ($item['link'] == 'false' && $item['type'] == false) {
-        $item['type'] == 'text';
-      }
-
-      $out .= "<li";
-      $attrs = array();
-      if ($item['type'] == 'current') {
-        $attrs['id'] = 'current';
-        //$item['link'] = false;
-      } elseif ($item['type'] == 'active' ||
-        ($item['link'] && Router::url($item['link']) == $this->_url)) {
-        $attrs['id'] = 'active';
-        $item['link'] = false;
-      } elseif ($item['type'] == 'multi') {
-        $attrs['class'] = 'multi';
-        $item['link'] = false;
-      }
-      foreach (array('onmouseover', 'onmouseout') as $event) {
-        if (isset($item[$event]))
-          $attrs[$event] = $item[$event];
-      }
-      foreach ($attrs as $name => $value) {
-        $out .= " $name=\"".htmlspecialchars($value)."\"";
-      }
-      $out .= ">";
-
-      if ($item['link']) {
-        $out .= $this->Html->link($item['text'], $item['link']);
-      } elseif ($item['type'] == 'text') {
-        $out .= "<span>".$item['text']."</span>";
-      } else {
-        $out .= $item['text'];
-      }
-
-      if(isset($item['submenu']))
-        $out .= $this->_getMenu($item['submenu']);
-
-      $out .= "</li>\n";
-    }
-    $out .= "</ul>\n";
-    return $out;
-  }
-
-  function getMainMenu($data) {
-    $data = am(array('id' => 'submenu', 'title' => __('Main Menu'), 'active' => false), $data);
-
-    // Get current url with action. If the default action 'index' is missing,
-    // its adds it to the url
-    $this->_url = $this->here;
-    if (strpos($this->_url, 'index') === false &&
-      strpos($this->action, 'index') !== false) {
-      if ($this->_url[strlen($this->_url)-1] != '/')
-        $this->_url .= '/';
-      $this->_url .= 'index';
-    }
-
-    return $this->output($this->_getMenu($data));
-  }
-
-  function _excludeKeys($data, $excluds = array('title', 'url', 'parent')) {
-    $filtered = array();
-    foreach ($data as $key => $value) {
-      if (is_numeric($key) || in_array($key, $excluds)) {
-        continue;
-      }
-      $filtered[$key] = $value;
-    }
-    return $filtered;
-  }
-
-  function _getSubMenu($menu, $options) {
-    if (!is_array($menu)) {
-      return false;
-    }
-    $items = array();
-    foreach ($menu as $key => $item) {
-      if (!is_numeric($key)) {
-        continue;
-      }
-      $attrs = $this->_excludeKeys($item);
-      $submenu = $this->_getSubMenu($item, $options);
-      $linkOptions = array();
-      if (isset($attrs['active']) && $attrs['active']) {
-        $linkOptions['class'] = 'active';
-      }
-      if (isset($item['url']) && $item['url'] === false) {
-        $item = $item['title'];
-      } else {
-        if (!isset($item['url'])) {
-          $item['url'] = array('controller' => $item['controller'], 'action' => $item['action'], 'admin' => $item['admin']);
-        }
-        $item = $this->Html->link($item['title'], $item['url'], $linkOptions);
-      }
-      $items[] = $this->Html->tag('li', $item . $submenu, $attrs);
-    }
-    if (count($items)) {
-      return $this->Html->tag('ul', implode("\n", $items), $options);
-    }
-    return false;
-  }
-
-  function menu($name, $options = array()) {
-    if (!isset($this->params['menus'][$name])) {
-      return false;
-    }
-    $menu = $this->params['menus'][$name];
-    return $this->_getSubMenu($menu, am($options, array('id' => $name), $menu['options']));
-  }
 
   /**
    * create item defaults
    *
    * @param array $items
+   * @return array of items with default values
    */
   function createItemDefaults(&$items) {
     $result = array();
@@ -272,6 +101,51 @@ class MenuHelper extends AppHelper
   }
 
   /**
+   * Create url from item
+   *
+   * @param array $item Menu item
+   * @return mixed
+   */
+  private function createUrl(&$item) {
+    $url = false;
+    if ($item['url']) {
+      $url = $item['url'];
+    } elseif ($item['controller']) {
+      $url = array('plugin' => $item['plugin'], 'controller' => $item['controller']);
+      if ($item['action']) {
+        $url['action'] = $item['action'];
+      } else {
+        $url['action'] = 'index';
+      }
+      if ($item['admin']) {
+        $url['admin'] = $item['admin'];
+      }
+    }
+    return $url;
+  }
+
+  /**
+   * Get html tag options for the link
+   *
+   * @param array $item Menu item
+   * @return array
+   */
+  private function getLinkOptions(&$item) {
+    $class = $item['class'];
+    if ($item['active']) {
+      $class[] = 'active';
+    } else if ($item['disabled']) {
+      $class[] = 'disabled';
+    }
+
+    $linkOptions = array();
+    if (count($class)) {
+      $linkOptions['class'] = join(' ', $class);
+    }
+    return $linkOptions;
+  }
+
+  /**
    * Render tree items
    *
    * @param array $treeItems
@@ -284,34 +158,12 @@ class MenuHelper extends AppHelper
         continue;
       }
 
-      $url = false;
-      if ($item['url']) {
-        $url = $item['url'];
-      } elseif ($item['controller']) {
-        $url = array('plugin' => $item['plugin'], 'controller' => $item['controller']);
-        if ($item['action']) {
-          $url['action'] = $item['action'];
-        } else {
-          $url['action'] = 'index';
-        }
-        if ($item['admin']) {
-          $url['admin'] = $item['admin'];
-        }
-      }
-      $class = $item['class'];
-      if ($item['active']) {
-        $class[] = 'active';
-      } else if ($item['disabled']) {
-        $class[] = 'disabled';
-      }
-      $aOptions = array();
-      if (count($class)) {
-        $aOptions['class'] = join(' ', $class);
-      }
+      $url = $this->createUrl($item);
+      $linkOptions = $this->getLinkOptions($item);
 
       $link = array();
       if ($url) {
-        $link[] = $this->Html->link($item['title'], $url, $aOptions);
+        $link[] = $this->Html->link($item['title'], $url, $linkOptions);
       } else {
         $link[] = $item['title'];
       }
